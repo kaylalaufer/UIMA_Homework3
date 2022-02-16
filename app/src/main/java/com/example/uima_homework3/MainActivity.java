b59b5a64dcab1d8df3ab6188f12a5d7d6e0d1c73
kaylalaufer <klaufer1@jhu.edu>
2022-02-15 23:56:18 -0500

    Main activity - started shared preference

--- app/src/main/java/com/example/uima_homework3/MainActivity.java
+++ app/src/main/java/com/example/uima_homework3/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.uima_homework3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String switchHR;
+    //public SharedPreferences sharedPreferences;
+    private Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch switch1 = findViewById(R.id.switch1);
+       // @SuppressLint("UseSwitchCompatOrMaterialCode")
+        switch1 = findViewById(R.id.switch1);
         switch1.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             if (isChecked) {
                 //Military Format
@@ -28,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void saveData () {
+        SharedPreferences sharedPreferences = getSharedPreferences("on/off", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(switchHR, switch1.isChecked());
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("on/off", MODE_PRIVATE);
+        //switchHR = sharedPreferences.getBoolean(switch1, false);
+    }
     public void onCalculationButtonClicked(View view) {
         Intent intent = new Intent(this, CalculationMode.class);
         startActivity(intent);

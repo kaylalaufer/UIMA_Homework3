5a3a5ef48ca720622707e60551ecd24bed73b445
kaylalaufer <klaufer1@jhu.edu>
2022-02-18 17:40:54 -0500

    added retry and home button

--- app/src/main/java/com/example/uima_homework3/LearnHard.java
+++ app/src/main/java/com/example/uima_homework3/LearnHard.java
@@ -2,6 +2,7 @@ package com.example.uima_homework3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,8 @@ public class LearnHard extends AppCompatActivity {
     private TextView correct;
     private TextView incorrect;
     private Button check;
+    private Button retry;
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,8 @@ public class LearnHard extends AppCompatActivity {
         correct = findViewById(R.id.textView36);
         incorrect = findViewById(R.id.textView37);
         check = findViewById(R.id.button4);
+        retry = findViewById(R.id.button5);
+        home = findViewById(R.id.button8);
 
         hours = findViewById(R.id.seekBar);
         minutes = findViewById(R.id.seekBar3);
@@ -91,14 +96,10 @@ public class LearnHard extends AppCompatActivity {
     }
 
     public void onCheck(View view) {
-        //String strHr = "" + userHr;
-        //String strMin = "" + userMin;
-        //inputHr.setText(strHr);
-        //inputMin.setText(strMin);
-        //inputHr.setVisibility(View.VISIBLE);
-        //inputMin.setVisibility(View.VISIBLE);
         int userHr = hours.getProgress();
         int userMin = minutes.getProgress();
+        retry.setVisibility(View.VISIBLE);
+        home.setVisibility(View.VISIBLE);
         if (userHr == correctHr && userMin == correctMin) {
             check.setVisibility(View.INVISIBLE);
             correct.setVisibility(View.VISIBLE);
@@ -106,5 +107,18 @@ public class LearnHard extends AppCompatActivity {
             check.setVisibility(View.INVISIBLE);
             incorrect.setVisibility(View.VISIBLE);
         }
+    }
+
+    public void onRetry (View view) {
+        check.setVisibility(View.VISIBLE);
+        incorrect.setVisibility(View.INVISIBLE);
+        correct.setVisibility(View.INVISIBLE);
+        retry.setVisibility(View.INVISIBLE);
+        home.setVisibility(View.INVISIBLE);
+    }
+
+    public void onHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
